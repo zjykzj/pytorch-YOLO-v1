a2fc14533b154efb5304a448bf8d169c9d3ebb68
zjkjzj <wy163zhuj@163.com>
2023-04-09 13:49:11 +0800

    perf(yololoss.py): update

--- yoloLoss.py
+++ yoloLoss.py
@@ -11,8 +11,8 @@ from torch.autograd import Variable
 class yoloLoss(nn.Module):
     def __init__(self, S, B, l_coord, l_noobj):
         super(yoloLoss, self).__init__()
-        self.S = S
-        self.B = B
+        # self.S = S
+        # self.B = B
         self.l_coord = l_coord
         self.l_noobj = l_noobj
 
@@ -55,41 +55,76 @@ class yoloLoss(nn.Module):
         target_tensor: (tensor) size(batchsize,S,S,30)
         '''
         N = pred_tensor.size()[0]
+        # 计算存在标注框的网格掩码
+        # [B, S, S, 1] -> [B, S, S]
         coo_mask = target_tensor[:, :, :, 4] > 0
+        # 计算不存在标注框的网格掩码
+        # [B, S, S, 1] -> [B, S, S]
         noo_mask = target_tensor[:, :, :, 4] == 0
+        # 掩码扩展到原始大小
+        # [B, S, S] -> [B, S, S, B*5+N_cls]
         coo_mask = coo_mask.unsqueeze(-1).expand_as(target_tensor)
+        # [B, S, S] -> [B, S, S, B*5+N_cls]
         noo_mask = noo_mask.unsqueeze(-1).expand_as(target_tensor)
 
+        # 通过掩码获取存在标注框的网格预测结果
+        # [B, S, S, B*5+N_cls] -> [N_coo, B*5+N_cls]
         coo_pred = pred_tensor[coo_mask].view(-1, 30)
+        # 获取预测框
+        # [N_coo, B*5+N_cls] -> [N_coo, B*5]
         box_pred = coo_pred[:, :10].contiguous().view(-1, 5)  # box[x1,y1,w1,h1,c1]
+        # 获取网格分类概率
+        # [N_coo, B*5+N_cls] -> [N_coo, N_cls]
         class_pred = coo_pred[:, 10:]  # [x2,y2,w2,h2,c2]
 
+        # 同样的，获取对应标注框信息
         coo_target = target_tensor[coo_mask].view(-1, 30)
         box_target = coo_target[:, :10].contiguous().view(-1, 5)
         class_target = coo_target[:, 10:]
 
         # compute not contain obj loss
+        # 首先计算不包含标注框的网格所在的损失，也就是该网格所有预测框的置信度应该趋近于0
+        # [N_coo, B*5+N_cls] -> [N_noo, B*5]
         noo_pred = pred_tensor[noo_mask].view(-1, 30)
+        # [N_coo, B*5+N_cls] -> [N_noo, B*5]
         noo_target = target_tensor[noo_mask].view(-1, 30)
+        # 获取预测框置度
         noo_pred_mask = torch.cuda.ByteTensor(noo_pred.size())
         noo_pred_mask.zero_()
-        noo_pred_mask[:, 4] = 1;
+        noo_pred_mask[:, 4] = 1
         noo_pred_mask[:, 9] = 1
+        # [N_noo, B*5] -> [N_noo * B]
         noo_pred_c = noo_pred[noo_pred_mask]  # noo pred只需要计算 c 的损失 size[-1,2]
+        # 同样的获取target
+        # [N_noo, B*5] -> [N_noo * B]
         noo_target_c = noo_target[noo_pred_mask]
+        # 计算均值平方差损失
         nooobj_loss = F.mse_loss(noo_pred_c, noo_target_c, reduction='sum')
 
         # compute contain obj loss
+        # 接下来计算包含标注框的网格损失，包括
+        # 1. 负责响应的预测框坐标损失
+        # 2. 负责响应的预测框置信度损失
+        # 3. 不负责响应的预测框置信度损失
+        # 4. 网格分类概率损失
+        #
+        # 创建响应掩码
         coo_response_mask = torch.cuda.ByteTensor(box_target.size())
         coo_response_mask.zero_()
+        # 创建不响应掩码
         coo_not_response_mask = torch.cuda.ByteTensor(box_target.size())
         coo_not_response_mask.zero_()
         box_target_iou = torch.zeros(box_target.size()).cuda()
         for i in range(0, box_target.size()[0], 2):  # choose the best iou box
             box1 = box_pred[i:i + 2]
             box1_xyxy = Variable(torch.FloatTensor(box1.size()))
+            # x1 = x_center / S. - 0.5 * box_w
+            # y1 = y_center / S. - 0.5 * box_h
+            # 那么它的预测结果（x_center/y_center）是相对于整幅图像的宽高
             box1_xyxy[:, :2] = box1[:, :2] / 14. - 0.5 * box1[:, 2:4]
             box1_xyxy[:, 2:4] = box1[:, :2] / 14. + 0.5 * box1[:, 2:4]
+            # 但是为什么target的标注框计算也是按照这种方式？
+            # 在数据集赋值过程中，target保存的坐标信息是标注框相对于网格的偏移比例，以及标注框相对于图像的宽高比例
             box2 = box_target[i].view(-1, 5)
             box2_xyxy = Variable(torch.FloatTensor(box2.size()))
             box2_xyxy[:, :2] = box2[:, :2] / 14. - 0.5 * box2[:, 2:4]
